7ea7e0ea41913567f360805f85d8d5baf9dfd04e
ericswetts <eric.swetts@gmail.com>
2020-08-06 04:05:11 +0400

    Add Create, Read, and Delete CRUD actions for Users model

--- COVID/backend/app.py
+++ COVID/backend/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///covid.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # DB
 db = SQLAlchemy(app)
 class Users(db.Model):
-    id = db.Column('user_id', db.Integer, primary_key = True) # primary_key makes it so that this value is unique and can be used to identify this record.
+    # primary_key makes it so that this value is unique and can be used to identify this record.
+    id = db.Column('user_id',db.Integer, primary_key=True)
     username = db.Column(db.String(24))
     email = db.Column(db.String(64))
     pwd = db.Column(db.String(64))
@@ -19,38 +21,42 @@ class Users(db.Model):
         self.pwd = pwd
 
 # routes!
-@app.route("/api/users", methods=["GET", "POST", "DELETE"])
 
+
+@app.route("/api/users", methods=["GET", "POST", "DELETE"])
 def users():
     method = request.method
-    if (method.lower() == "get"): # READ
+    if (method.lower() == "get"):  # READ
         users = Users.query.all()
-        return jsonify([{"id": i.id, "username": i.username, "email": i.email, "password": i.pwd} for i in users]) # Get all values from db
-    elif (method.lower() == "post"): # CREATE
+        # Get all values from db
+        return jsonify([{"id": i.id, "username": i.username, "email": i.email, "pwd": i.pwd} for i in users])
+    elif (method.lower() == "post"):  # CREATE
         try:
             username = request.json["username"]
             email = request.json["email"]
             pwd = request.json["pwd"]
-            if (username and pwd and email): # Checks if username, pwd or email are empty
+            if (username and pwd and email):  # Checks if username, pwd or email are empty
                 try:
-                    user = Users(username, email, pwd) # Creates a new record
-                    db.session.add(user) # Adds the record for committing
-                    db.session.commit() # Saves our changes
-                    return jsonify({"success": True})
+                    user = Users(username, email, pwd)  # Creates a new record
+                    db.session.add(user)  # Adds the record for committing
+                    db.session.commit()  # Saves our changes
+                    return jsonify({"success": True, "cool": "beans"})
                 except Exception as e:
                     return ({"error": e})
             else:
-                return jsonify({"error": "Invalid form"}) # jsonify converts python vars to json
+                # jsonify converts python vars to json
+                return jsonify({"error": "Invalid form"})
         except:
             return jsonify({"error": "Invalid form"})
-    elif (method.lower() == "delete"): # DESTROY
+    elif (method.lower() == "delete"):  # DESTROY
         try:
             uid = request.json["id"]
             if (uid):
                 try:
-                    user = Users.query.get(uid) # Gets user with id = uid (because id is primary key)
-                    db.session.delete(user) # Delete the user
-                    db.session.commit() # Save
+                    # Gets user with id = uid (because id is primary key)
+                    user = Users.query.get(uid)
+                    db.session.delete(user)  # Delete the user
+                    db.session.commit()  # Save
                     return jsonify({"success": True})
                 except Exception as e:
                     return jsonify({"error": e})
@@ -58,3 +64,14 @@ def users():
                 return jsonify({"error": "Invalid form"})
         except:
             return jsonify({"error": "m"})
+                # return ({"error": "Invalid form"})
+    # except:
+    #         return ({"error": "Invalid form"})
+
+# @app.route("/")
+# def index():
+#     db.create_all()
+#     return("hello squirrels")
+
+if __name__ == "__main__":
+    app.run(debug=True)
