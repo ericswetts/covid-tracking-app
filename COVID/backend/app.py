64d00b6d552d43f332d1b6381ce75d9bfd804c39
ericswetts <eric.swetts@gmail.com>
2020-08-06 11:41:33 +0400

    Add Login and Register componentds. Add axios for promise handling. Add flask-cors for CORS handling.

--- COVID/backend/app.py
+++ COVID/backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///covid.db"
@@ -20,9 +21,7 @@ class Users(db.Model):
         self.email = email
         self.pwd = pwd
 
-# routes!
-
-
+# Routes!
 @app.route("/api/users", methods=["GET", "POST", "DELETE"])
 def users():
     method = request.method
@@ -70,6 +69,79 @@ def users():
 # def index():
 #     db.create_all()
 #     return("hello squirrels")
+
+
+
+#functions to connect backend to frontend?
+def getUsers():
+    users = Users.query.all()
+    return [{"id": i.id, "username": i.username, "email": i.email, "password": i.pwd} for i in users]
+
+def addUser(username, email, pwd):
+    if (username and pwd and email):
+        try:
+            user = Users(username, email, pwd)
+            db.session.add(user)
+            db.session.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    else:
+        return False
+
+def removeUser(uid):
+    uid = request.json["id"]
+    if (uid):
+        try:
+            user = Users.query.get(uid)
+            db.session.delete(user)
+            db.session.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    else:
+        return False
+
+
+#Login and register check
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    try:
+        email = request.json["email"]
+        password = request.json["pwd"]
+        if (email and password):
+            users = getUsers()
+            # Check if user exists
+            return jsonify(len(list(filter(lambda x: x["email"] == email and x["password"] == password, users))) == 1)
+        else:
+            return jsonify({"error": "invalid form"})
+    except:
+        return jsonify({"error": "Invalid form"})
+
+@app.route("/api/register", methods=["POST"])
+def register():
+    users = getUsers()
+    try:
+        email = request.json["email"]
+        email = email.lower()
+        password = request.json["pwd"]
+        username = request.json["username"]
+        # Check to see if user already exists
+        users = getUsers()
+
+        if(len(list(filter(lambda x: x["email"] == email, users))) == 1):
+            return jsonify({"error": "not unique email"})
+        # Email validation check
+        if not re.match(r"[\w\._]{5,}@\w{3,}.\w{2,4}", email):
+            return jsonify({"error": "regex thing"})
+        addUser(username, email, password)
+        return jsonify({"success": True})
+    except:
+        return jsonify({"error": "Something else is broken"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ COVID/frontend/src/components/Login.jsx
@@ -0,0 +1,54 @@
+// Login.jsx
+import React, { Component } from "react";
+import axios from "axios";
+
+class Login extends Component {
+    login = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:5000/api/login", {
+                email: document.getElementById("email").value,
+                pwd: document.getElementById("password").value,
+            })
+            .then((res) => {
+                console.log(res.data);
+            });
+    };
+
+    render() {
+        return (
+            <div className="w3-card-4" style={{ margin: "2rem" }}>
+                <div className="w3-container w3-blue w3-center w3-xlarge">
+                    LOGIN
+                </div>
+                <div className="w3-container">
+                    <form onSubmit={this.login}>
+                        <p>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                className="w3-input w3-border"
+                                id="email"
+                            />
+                        </p>
+                        <p>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                className="w3-input w3-border"
+                                id="password"
+                            />
+                        </p>
+                        <p>
+                            <button type="submit" class="w3-button w3-blue">
+                                Login
+                            </button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
